c6fe469cac3b3259e7fd08edc32fc632ba63b5d0
Taimoor Arshad <taimoor.arshad43@gmail.com>
2024-08-11 16:08:11 -0400

    Adding gameboard route

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 from boggle import Boggle
-from flask import Flask, render, redirect, session
+from flask import Flask, render_template, redirect, session
 
 boggle_game = Boggle()
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = 'secret'
+
+@app.route("/")
+def index():
+    board = boggle_game.make_board()
+    print(board)
+    return render_template("index.html", board = board)
