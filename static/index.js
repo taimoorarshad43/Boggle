c0f5399ff777aa9c8150eeebf0bc87cb5aadbe28
Taimoor Arshad <taimoor.arshad43@gmail.com>
2024-08-12 14:30:21 -0400

    Comments and code readibility

--- static/index.js
+++ static/index.js
@@ -2,12 +2,15 @@ let result = '';
 
 $('form').on("submit", async function(e) {
     e.preventDefault();
+
     let $input = $('input');
     let $guess = $input.val();
     console.log($guess);
-    result = await axios.get("/guess", {params : {guess : $guess}}) // difference using GET/POST?
+
+    result = await axios.get("/guess", {params : {guess : $guess}}) // 
     result = result.data;
     console.log(result);
+
     $input.val("")
     $("div.result").text("")
     $("body").append(`<div class = "text-center mt-5 result">
